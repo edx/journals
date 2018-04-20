9a1f58b6840d326ecda6e9f483cfc1de1b5f9872
Bill Filler <bfiller@edx.org>
2018-04-20 13:26:56 -0400

    Add serializers for Journal and JournalAboutPage

--- journals/apps/api/serializers.py
+++ journals/apps/api/serializers.py
@@ -1,14 +1,34 @@
 '''Journal serializers'''
 from rest_framework import serializers
 from journals.apps.core.models import User
-from journals.apps.journals.models import Journal, JournalAccess
+from journals.apps.journals.models import Journal, JournalAccess, JournalAboutPage
+
+
+class JournalAboutPageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for JournalAboutPage
+    """
+    class Meta:
+        model = JournalAboutPage
+        fields = ('slug', 'card_image_absolute_url', 'short_description')
+
+
+class JournalSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Journal
+    """
+    journalaboutpage = JournalAboutPageSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Journal
+        fields = ('name', 'journalaboutpage')
 
 
 class JournalAccessSerializer(serializers.ModelSerializer):
     """
     Serializer for the ``JournalAccess`` model.
     """
-    journal = serializers.SlugRelatedField(slug_field='name', queryset=Journal.objects.all())
+    journal = JournalSerializer(many=False, read_only=True)
     user = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
 
     class Meta(object):
