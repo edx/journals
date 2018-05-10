92a2ec1c673e340bbbdec57495f6eb81d8a8ce4e
Matt Tuchfarber <mtuchfarber@edx.org>
2018-05-10 14:49:18 -0400

    Fix logging regression in docker
    The recent changes to the logging settings in #33 broke logging in the docker devstack since
    docker doesn't support the syslog socket. Copied code from ecommerce:
    https://github.com/edx/ecommerce/blob/master/ecommerce/settings/devstack.py#L11

--- journals/settings/local.py
+++ journals/settings/local.py
@@ -52,6 +52,11 @@ SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
 
 ENABLE_AUTO_AUTH = True
 
+# Docker does not support the syslog socket at /dev/log. Rely on the console.
+LOGGING['handlers']['local'] = {
+    'class': 'logging.NullHandler',
+}
+
 #####################################################################
 # Lastly, see if the developer has any local overrides.
 if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
