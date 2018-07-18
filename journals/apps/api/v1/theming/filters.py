b258032cdb41fc0f26c7f7eef1bfd22e386168ab
Bessie Steinberg <bsteinberg@edx.org>
2018-07-18 11:01:36 -0400

    Add frontend_url query param to sitebranding API
    - Add query param 'frontend_url' to sitebranding API.  'frontend_url' is
      a field on the 'SiteConfiguration' table which is associated with a
      'Site'.  When 'frontend_url' is set it will return the 'SiteBranding'
      record associated with the same 'Site'.
    - Made 'frontend_url' unique so it can be used to query specific site
      brandings.
    - Removed extra returned fields and query fields that we don't yet have
      a use case for.

--- journals/apps/api/v1/theming/filters.py
+++ journals/apps/api/v1/theming/filters.py
@@ -2,17 +2,27 @@
 from django_filters import rest_framework as filters
 
 from journals.apps.theming.models import SiteBranding
+from journals.apps.core.models import SiteConfiguration
 
 
 class SiteBrandingFilter(filters.FilterSet):
     """ Filter for SiteBranding """
-    site_name = filters.CharFilter(name='site__site_name')
-    hostname = filters.CharFilter(name='site__hostname')
+    frontend_url = filters.CharFilter(name='frontend_url', method='filter_frontend_url')
+
+    def filter_frontend_url(self, queryset, name, value):  # pylint: disable=unused-argument
+        """
+        Return SiteBranding records associated with SiteConfigurations with this frontend_url
+        """
+        # SiteBranding is associated with a Site and a SiteConfiguration is associated with a Site.
+        # Find the SiteConfiguration with this frontend_url.
+        # 'frontend_url' is a unique field so there should only by one site_configuration
+        site_configuration = SiteConfiguration.objects.get(frontend_url=value)
+
+        # Return the SiteBranding associated with the same Site.
+        return queryset.filter(site_id=site_configuration.site_id)
 
     class Meta:
         model = SiteBranding
         fields = [
-            'theme_name',
-            'site_name',
-            'hostname',
+            'frontend_url'
         ]
