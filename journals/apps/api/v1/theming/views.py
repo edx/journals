4f6989c38a18e772b290306b041b9850311f909b
Bessie Steinberg <bsteinberg@edx.org>
2018-08-09 14:43:00 -0400

    Allow Any on SiteBranding API

--- journals/apps/api/v1/theming/views.py
+++ journals/apps/api/v1/theming/views.py
@@ -1,6 +1,7 @@
 """ API for theming / site branding """
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from journals.apps.api.v1.theming.filters import SiteBrandingFilter
 from journals.apps.api.v1.theming.serializers import SiteBrandingSerializer
@@ -13,3 +14,4 @@ class SiteBrandingViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SiteBranding.objects.all()
     filter_backends = (DjangoFilterBackend, )
     filter_class = SiteBrandingFilter
+    permission_classes = (AllowAny, )
