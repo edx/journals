2e91cc23138d2b7d033696518a7ec8140956742f
Matt Tuchfarber <matt@tuchfarber.com>
2018-09-07 14:45:36 -0400

    Fix

--- journals/apps/journals/blocks.py
+++ journals/apps/journals/blocks.py
@@ -1,11 +1,13 @@
 """ Custom blocks """
+from urllib.parse import urljoin
+
 from bs4 import BeautifulSoup as parser
 from django.utils import six
 from django.utils.safestring import mark_safe
 from wagtail.wagtailcore import blocks
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
-from wagtail.wagtailcore.rich_text import expand_db_html
+from wagtail.wagtailcore.rich_text import extract_attrs, get_link_handler, get_embed_handler, FIND_A_TAG, FIND_EMBED_TAG
 
 from journals.apps.journals.models import Video
 from journals.apps.journals.widgets import AdminVideoChooser
@@ -84,8 +86,30 @@ class JournalRichTextBlock(blocks.RichTextBlock):
     def get_searchable_content(self, value):
         return [parser(value.source, 'html.parser').get_text(' ')]
 
+    def expand_db_html(self, html, for_editor=False, base_url='/'):
+        def replace_a_tag(m):
+            attrs = extract_attrs(m.group(1))
+            if 'linktype' not in attrs:
+                # return unchanged
+                return m.group(0)
+            handler = get_link_handler(attrs['linktype'])
+            return handler.expand_db_attributes(attrs, for_editor)
+
+        def replace_embed_tag(m):
+            attrs = extract_attrs(m.group(1))
+            handler = get_embed_handler(attrs['embedtype'])
+            html = handler.expand_db_attributes(attrs, for_editor)
+            if attrs['embedtype'] == 'image':
+                html.replace('src="/', 'src="{}'.format(base_url))
+            return html
+
+        html = FIND_A_TAG.sub(replace_a_tag, html)
+        html = FIND_EMBED_TAG.sub(replace_embed_tag, html)
+        return html
+
     def get_api_representation(self, value, context=None):
-        return expand_db_html(value.source)
+        base_url = urljoin(context['request'].build_absolute_uri(), '/')
+        return self.expand_db_html(value.source, base_url=base_url)
 
 
 class JournalRawHTMLBlock(blocks.RawHTMLBlock):
