6cb33dcf6bca4e58961ed2228995b6a91f0f68d4
Zia Fazal <zia.fazal@arbisoft.com>
2018-11-09 12:53:45 +0500

    set BASE_URL setting by geting scheme and host from request

--- journals/apps/core/tests/test_middleware.py
+++ journals/apps/core/tests/test_middleware.py
@@ -1,6 +1,6 @@
 """ Test Cases for middleware """
 import ddt
-from mock import patch
+from mock import PropertyMock, patch
 
 from django.conf import settings
 from django.test import TestCase
@@ -26,7 +26,7 @@ class TestSettingsMiddleware(TestCase):
     @ddt.unpack
     def test_notification_settings_override(self, hostname, from_email):
         """
-        Test WAGTAILADMIN_NOTIFICATION_FROM_EMAIL setting override base on site
+        Test WAGTAILADMIN_NOTIFICATION_FROM_EMAIL setting override based on site
         """
         site = SiteFactory(hostname=hostname)
         site_configuration = SiteConfigurationFactory(site=site, from_email=from_email)
@@ -36,3 +36,22 @@ class TestSettingsMiddleware(TestCase):
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(settings.WAGTAILADMIN_NOTIFICATION_FROM_EMAIL, site_configuration.from_email)
+
+    @ddt.data(
+        ('http', 'example.com'),
+        ('https', 'dummy.org'),
+        ('ftp', 'test.co.name'),
+    )
+    @ddt.unpack
+    @patch('django.core.handlers.wsgi.WSGIRequest.get_host')
+    @patch('django.core.handlers.wsgi.WSGIRequest.scheme', new_callable=PropertyMock)
+    def test_base_url_setting_override(self, scheme, hostname, mock_scheme, mock_host):
+        """
+        Test BASE_URL setting override
+        """
+        url = "{}://{}".format(scheme, hostname)
+        mock_scheme.return_value = scheme
+        mock_host.return_value = hostname
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(settings.BASE_URL, url)
